c07621b3e6a2bccea598c7dc4658c3f2b3762e49
a840900544 <67825726+a840900544@users.noreply.github.com>
2024-09-22 14:07:55 +0800

    Update loon_bd.js

--- Proxy/loon_bd.js
+++ Proxy/loon_bd.js
@@ -115,6 +115,6 @@ function _writeHttpHeader() {
   let conPort = $session.conPort;
   let verify = createVerify(conHost);
 
-  var header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\nHost: ${conHost}:${conPort}\r\nX-T5-Auth: ${verify}\r\nProxy-Connection: keep-alive\r\n\r\n`;
-  $tunnel.write($session, header);
+var header = `CONNECT ${conHost}:${conPort}HTTP/1.1\r\nHost: 153.3.236.22:443\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1 baiduboxapp\r\nX-T5-Auth: 683556433\r\nProxy-Connection: keep-alive\r\n\r\n`; 
+     $tunnel.write($session, header);
 }
